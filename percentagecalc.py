dd6dd2a3619e6b1f7687cd1f88673b65a870b59d
Ayaan-Singh <asthana.divya@gmail.com>
2024-10-29 19:19:36 +0530

    hi

---
+++ percentagecalc.py
@@ -0,0 +1,12 @@
+math = int(input("math score"))
+eng = int(input("eng score"))
+sci = int(input("sci score"))
+hindi = int(input("hindi score"))
+
+
+sum = math+eng+sci+hindi
+print ("the sum of all of the subjects is",sum)
+
+perc = (sum/400)*100
+
+print ( "percetage score \n:" ,perc)
